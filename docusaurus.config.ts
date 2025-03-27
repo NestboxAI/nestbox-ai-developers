df5cb52bd9cf1a0dec7cb6ff8e49b397d5be4b47
Roy Ganor <ganoro@gmail.com>
2025-03-27 16:44:05 -0400

    Update docusaurus.config.ts

--- docusaurus.config.ts
+++ docusaurus.config.ts
@@ -110,7 +110,7 @@ const config: Config = {
         },
         { to: "/blog", label: "Blog", position: "left" },
         {
-          href: "https://github.com/facebook/docusaurus",
+          href: "https://github.com/NestboxAI/nestbox-ai-clients",
           label: "GitHub",
           position: "right",
         },
@@ -154,7 +154,7 @@ const config: Config = {
             },
             {
               label: "GitHub",
-              href: "https://github.com/facebook/docusaurus",
+              href: "https://github.com/NestboxAI/nestbox-ai-clients",
             },
           ],
         },
